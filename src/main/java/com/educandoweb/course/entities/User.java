332653c89a26c54f49c6114190a5bbbc10ec9e3e
Lucas Eufrásio <lucas_hyan@yahoo.com.br>
2026-05-20 14:27:40 -0300

    Adicionando Lógica e Relacionamento Básico a Entidade Order

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -23,6 +25,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    //------------------------------------------------------
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
 
@@ -76,6 +81,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -87,4 +96,7 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(getId());
     }
+
+
+
 }
